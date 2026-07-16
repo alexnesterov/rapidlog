f663baa38ee3947e32039b2e93f7abf50b062abf
Alex Nesterov <ialexnesterov@gmail.com>
2026-07-16 11:03:32 +0300

    Show query params and headers in greet handler response

--- internal/adapter/httpapi/greet_handler.go
+++ internal/adapter/httpapi/greet_handler.go
@@ -5,13 +5,33 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 	"time"
 )
 
 func Greet(w http.ResponseWriter, r *http.Request) {
-	today := time.Now().Format("02.01 Mon 06")
+	result := &strings.Builder{}
 
-	if _, err := fmt.Fprintf(w, "%s\nHello World!", today); err != nil {
+	today := time.Now().Format("02.01 Mon 06")
+	fmt.Fprintf(result, "%s\n\n", today)
+
+	agent := r.Header.Get("User-Agent")
+	fmt.Fprintf(result, "User-Agent: %s\n\n", agent)
+
+	values := r.URL.Query()
+	fmt.Fprintf(result, "%s\n", values)
+
+	for k, v := range values {
+		fmt.Fprintf(result, "%s: %s\n", k, v)
+	}
+
+	name := values.Get("name")
+	if name == "" {
+		name = "world"
+	}
+	fmt.Fprintf(result, "\nHello, %s!\n\n", name)
+
+	if _, err := w.Write([]byte(result.String())); err != nil {
 		log.Println(err)
 	}
 }
